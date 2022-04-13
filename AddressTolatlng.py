58379929fa828fb7765518bd9c76963fed514bb6
msvish <49274594+msvish@users.noreply.github.com>
2022-04-14 01:38:18 +0530

    Create AddressTolatlng.py

---
+++ AddressTolatlng.py
@@ -0,0 +1,50 @@
+from flask import Flask, request , jsonify
+import simplexml
+import requests
+
+app=Flask(__name__)
+
+@app.route('/getAddressDetails', methods=['POST'])
+def addressTOlatlng():
+    request_data=request.get_json()
+    address=request_data['address']
+    output_format=request_data['output_format']
+    
+    
+    API_KEY='-----------API_KEY------------'
+    
+    params={
+    'key':API_KEY,
+    'address':address
+    }
+
+    base_url='https://maps.googleapis.com/maps/api/geocode/json?'
+
+    response=requests.get(base_url, params=params)
+    response=response.json()
+
+    if response['status']=='OK':
+        lat=response['results'][0]['geometry']['location']['lat']
+        lng=response['results'][0]['geometry']['location']['lng']
+        if output_format=='xml':        
+            response=simplexml.dumps(
+                {"root":{
+                    "address":address,
+                    "cordinates":{
+                        "lat":lat,
+                        "lng":lng
+                    }
+                } })
+        else:
+             response= jsonify(
+                Address=address,
+                coordinates={
+                    "lat":lat,
+                    "lng":lng
+                }            
+            )
+    return response
+        
+
+if __name__=='__main__':
+    app.run()
